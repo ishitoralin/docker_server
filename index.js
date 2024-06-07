dd8e7166bdce19b6b7d712b7c934e1fe08ccd3e2
ishitoralin <miraculous0218@hotmail.com>
2024-06-07 10:59:59 +0800

    build docker api

--- index.js
+++ index.js
@@ -2,38 +2,38 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
-app.get('/',(req,res)=>{
+
+const basicroute = require('./routes/basicRoute')
+const dockerInfo = require('./routes/dockerInfo')
+
+app.get('/', (req, res) => {
     res.type('text/plain')
-    res.send('homeadas')
+    res.send('server is on 123')
 })
 
-app.get('/about',(req,res)=>{
-    res.type('text/plain')
-    res.send('about')
-})
-
-// app.use(express.static(__dirname+'/public')
+app.use('/basic', basicroute)
+app.use('/info', dockerInfo)
 
 // cors: 3000 for fronted-end, 8080 for database(MySQL)
-const whitelist = ['http://localhost:3000/','http://localhost:8080/']
+const whitelist = ['http://localhost:3000/', 'http://localhost:8080/']
 app.use(
     cors({
-      origin: whitelist,
-      credentials: true,
+        origin: whitelist,
+        credentials: true,
     })
-  );
+);
 
-app.use((err, req, res,next)=>{
+app.use((err, req, res, next) => {
     console.log(err.message)
     res.status(500)
     res.send('500 - Server Error')
 })
 
-
-app.use('*',(req,res)=>{
-    res.status(404).json({message:'the page does not exist'})
+app.use('*', (req, res) => {
+    res.status(404).json({ message: 'the page does not exist' })
 })
+
 const port = process.env.PORT || 3001
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Express started on http://localhost:${port}`)
 })
